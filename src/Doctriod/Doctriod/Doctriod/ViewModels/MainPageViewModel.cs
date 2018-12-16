7ed285566f6b21e08eb088de0191ade79e068be7
Pieter van der Westhuizen <pieter@csoft.co.za>
2018-12-16 11:21:23 +0200

    Initial Project Layout

---
+++ src/Doctriod/Doctriod/Doctriod/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Doctriod.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+            //FontName.Text = PageHeading.FontFamily;
+        }
+    }
+}
