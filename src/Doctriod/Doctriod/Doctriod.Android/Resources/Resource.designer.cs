f6488a6927d3123b06e201b7533c7ad9a9f6d9f1
Pieter van der Westhuizen <pieter@csoft.co.za>
2018-12-20 10:39:00 +0200

    Home Page with icons

--- src/Doctriod/Doctriod/Doctriod.Android/Resources/Resource.designer.cs
+++ src/Doctriod/Doctriod/Doctriod.Android/Resources/Resource.designer.cs
@@ -3947,7 +3947,13 @@ namespace Doctriod.Droid
 			public const int home = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int ic_launcher = 2130903042;
+			public const int hospital = 2130903042;
+			
+			// aapt resource value: 0x7f030003
+			public const int ic_launcher = 2130903043;
+			
+			// aapt resource value: 0x7f030004
+			public const int surgeon = 2130903044;
 			
 			static Mipmap()
 			{
---
+++ src/Doctriod/Doctriod/Doctriod/Controls/DoctroidPictureButton.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Doctriod.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DoctroidPictureButton : ContentView
+    {
+        public DoctroidPictureButton()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(
+            propertyName: "Text",
+            returnType: typeof(string),
+            declaringType: typeof(DoctroidPictureButton),
+            defaultValue: "",
+            defaultBindingMode: BindingMode.TwoWay,
+            propertyChanged: TextPropertyChanged);
+
+        public string Text
+        {
+            get { return base.GetValue(TextProperty).ToString(); }
+            set { base.SetValue(TextProperty, value); }
+        }
+
+        private static void TextPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (DoctroidPictureButton)bindable;
+            control.ButtonLabel.Text = newValue.ToString();
+        }
+
+        public static readonly BindableProperty IconProperty = BindableProperty.Create(
+            propertyName: "Icon",
+            returnType: typeof(string),
+            declaringType: typeof(DoctroidPictureButton),
+            defaultValue: "",
+            defaultBindingMode: BindingMode.TwoWay,
+            propertyChanged: IconPropertyChanged);
+
+        public string Icon
+        {
+            get { return base.GetValue(IconProperty).ToString(); }
+            set { base.SetValue(IconProperty, value); }
+        }
+
+        private static void IconPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (DoctroidPictureButton)bindable;
+            control.ButtonImage.Source = newValue.ToString();
+
+        }
+    }
+}
