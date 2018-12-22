bdbaea39d68800795994b6ef571df2e981cd834a
Pieter van der Westhuizen <pieter@csoft.co.za>
2018-12-22 17:42:11 +0200

    Gradient background drawer menu

---
+++ src/Doctriod/Doctriod/Doctriod.Android/Renderers/GradientContentPageRenderer.cs
@@ -0,0 +1,45 @@
+﻿using Android.Content;
+using Doctriod.Droid.Renderers;
+using Doctriod.NavPage;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(GradientContentPage), typeof(GradientContentPageRenderer))]
+namespace Doctriod.Droid.Renderers
+{
+    public class GradientContentPageRenderer : PageRenderer
+    {
+        public GradientContentPageRenderer(Context context) : base(context) { }
+
+        private Xamarin.Forms.Color StartColor { get; set; }
+        private Xamarin.Forms.Color EndColor { get; set; }
+        protected override void DispatchDraw(
+            global::Android.Graphics.Canvas canvas)
+        {
+            var gradient = new Android.Graphics.LinearGradient(0, 0, 0, Height,
+                this.StartColor.ToAndroid(),
+                this.EndColor.ToAndroid(),
+                Android.Graphics.Shader.TileMode.Mirror);
+            var paint = new Android.Graphics.Paint()
+            {
+                Dither = true,
+            };
+            paint.SetShader(gradient);
+            canvas.DrawPaint(paint);
+            base.DispatchDraw(canvas);
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Page> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null || Element == null)
+            {
+                return;
+            }
+            var page = e.NewElement as GradientContentPage;
+            this.StartColor = page.StartColor;
+            this.EndColor = page.EndColor;
+        }
+    }
+}
