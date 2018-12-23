ed4191b8397ec6394af68b8663b4a76c9bf9eeca
Pieter van der Westhuizen <pieter@csoft.co.za>
2018-12-23 15:48:19 +0200

    Drawer menu

---
+++ src/Doctriod/Doctriod/Doctriod/Models/MenuItem.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Doctriod.Models
+{
+    public class MenuItem
+    {
+        public string Title { get; set; }
+        public string Page { get; set; }
+        public bool IsActive { get; set; }
+
+        public bool IsNotActive
+        {
+            get { return !IsActive; }
+        }
+
+        public bool IsFirst { get; set; }
+
+        public bool MustBeLoggedIn { get; set; }
+    }
+}
--- src/Doctriod/Doctriod/Doctriod/ViewModels/DrawerMenuPageViewModel.cs
+++ src/Doctriod/Doctriod/Doctriod/ViewModels/DrawerMenuPageViewModel.cs
@@ -2,15 +2,36 @@
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using Doctriod.Models;
 
 namespace Doctriod.ViewModels
 {
-	public class DrawerMenuPageViewModel : BindableBase
-	{
+    public class DrawerMenuPageViewModel : BindableBase
+    {
+        private ObservableCollection<MenuItem> _menuItems;
+        public ObservableCollection<MenuItem> MenuItems
+        {
+            get => _menuItems;
+            set => SetProperty(ref _menuItems, value);
+        }
+
         public DrawerMenuPageViewModel()
         {
-
+            InitMenu();
         }
-	}
+
+        private void InitMenu()
+        {
+            MenuItems = new ObservableCollection<MenuItem>();
+            MenuItems.Add(new MenuItem() { Title = "HOME", Page = "Home", IsActive = false, IsFirst = true, MustBeLoggedIn = false });
+            MenuItems.Add(new MenuItem() { Title = "DOCTORS", Page = "Doctors", IsActive = false, MustBeLoggedIn = false });
+            MenuItems.Add(new MenuItem() { Title = "HOSPITALS", Page = "Hospitals", IsActive = true, MustBeLoggedIn = false });
+            MenuItems.Add(new MenuItem() { Title = "INSURANCE", Page = "Insurance", IsActive = false, MustBeLoggedIn = false });
+            MenuItems.Add(new MenuItem() { Title = "HISTORY", Page = "History", IsActive = false, MustBeLoggedIn = false });
+            MenuItems.Add(new MenuItem() { Title = "MEDICINE", Page = "Medicine", IsActive = false, MustBeLoggedIn = false });
+            MenuItems.Add(new MenuItem() { Title = "LOG OUT", Page = "LogOut", IsActive = false, MustBeLoggedIn = false });
+        }
+    }
 }
